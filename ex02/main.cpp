8ab3af9dd2f23e52077f6b0e01631190acd35f99
Nazrin shah Fernandez <nfernand@u82z06s01.42kl.edu.my>
2022-06-23 15:52:25 +0800

    added more test cases

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,11 +6,13 @@
 /*   By: nfernand <nfernand@student.42kl.edu.m      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 17:22:04 by nfernand          #+#    #+#             */
-/*   Updated: 2022/05/30 18:06:29 by nfernand         ###   ########.fr       */
+/*   Updated: 2022/06/23 15:27:13 by nfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <random>
+#include <time.h>
 
 #include "Base.hpp"
 #include "A.hpp"
@@ -22,21 +24,68 @@
 using	std::cout;
 using	std::endl;
 
+Base	*generate(void)
+{
+	srand(time(0));
+	int	random = rand() % 99;
+
+	if (random <= 33)
+		return (new A());
+	else if (random <= 66)
+		return (new B());
+	else
+		return (new C());
+	return (NULL);
+}
+
+void	identify(Base *p)
+{
+	cout << BLUE "identifying with using a pointer" << endl;
+	if (dynamic_cast<A*>(p) != nullptr)
+		cout << GREEN "<A subclass>" RESET << endl;
+	if (dynamic_cast<B*>(p) != nullptr)
+		cout << GREEN "<B subclass>" RESET << endl;
+	if (dynamic_cast<C*>(p) != nullptr)
+		cout << GREEN "<C subclass>" RESET << endl;
+}
+
+void	identify(Base &p)
+{
+	//void just to get past norm
+	cout << BLUE "identifying without using a pointer" << endl;
+	try
+	{
+		(void)dynamic_cast<A&>(p);
+		cout << GREEN "<A subclass>" RESET << endl;
+	}
+	catch(std::bad_cast){}
+	try
+	{
+		(void)dynamic_cast<B&>(p);
+		cout << GREEN "<B subclass>" RESET << endl;
+	}
+	catch(std::bad_cast){}
+	try
+	{
+		(void)dynamic_cast<C&>(p);
+		cout << GREEN "<C subclass>" RESET << endl;
+	}
+	catch(std::bad_cast){}
+}
+
 int	main()
 {
-	Base	*base = new Base();
-//	Base	*a = new A();
-//	Base	*b = new B();
-//	Base	*c = new C();
+	cout << MAGENTA "Generating a random derived class..." RESET << endl;
+	Base	*new_base = generate();
+	cout << endl;
 
-	Base	*new_base = base->generate();
+	cout << MAGENTA "Printing the type of derived class using when passed as pointer..." RESET << endl;
 	new_base->identify(new_base);
+	cout << endl;
+
+	cout << MAGENTA "Printing the type of derived class using when passed as reference..." RESET << endl;
 	new_base->identify(*new_base);
 
-//	delete c;
-//	delete b;
-//	delete a;
 	delete new_base;
-	delete base;
 	return (0);
 }
