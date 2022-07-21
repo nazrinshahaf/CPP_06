328677db2ce7e757396d96d9c9f0f69395b02fe0
Nazrin shah Fernandez <nfernand@u82z06s01.42kl.edu.my>
2022-07-21 11:28:01 +0800

    changed naming convetion ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nfernand <nfernand@student.42kl.edu.m      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 11:07:43 by nfernand          #+#    #+#             */
-/*   Updated: 2022/06/22 14:13:55 by nfernand         ###   ########.fr       */
+/*   Updated: 2022/07/21 11:27:02 by nfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,14 +50,14 @@ int		main()
 	cout << endl;
 
 	cout << MAGENTA "Deserializing Data..." RESET << endl;
-	Data	*reversedtest = deserialization(serial);
+	Data	*deserial = deserialization(serial);
 	cout << MAGENTA "Printing value of serialized data..." RESET << endl;
-	cout << reversedtest << endl;
-	cout << *reversedtest << endl;
+	cout << deserial << endl;
+	cout << *deserial << endl;
 
 	cout << MAGENTA "Comparing initial and start values..." RESET << endl;
 	cout << "Data is ";
-	if (data == reversedtest)
+	if (data == deserial)
 		cout << "the" GREEN " same" RESET;
 	else
 		cout << RED "not the same" RESET;
